c69d54ab258365275fd0e17255d04345108dbe65
Ben Kinsey <bkinsey@xtivia.com>
2020-03-10 00:42:44 -0500

    can read json data from script tag

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,19 @@ import App from './App';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const window: any;
 
+const getReactAppData = (element: Element): {} => {
+  const jsonId = element.getAttribute('data-json-id') ?? '';
+  if (jsonId) {
+    const content = String(document.getElementById(jsonId)?.innerHTML ?? '');
+    return JSON.parse(content);
+  }
+  return JSON.parse(element.getAttribute('data-custom') ?? '');
+};
+
 window.invokeReact = (): void => {
-  document.querySelectorAll('.react-app').forEach((element, i) => {
-    setTimeout(() => {
-      const appName = String(element.getAttribute('data-app'));
-      const appData = JSON.parse(String(element.getAttribute('data-custom')));
-      ReactDOM.render(<App appName={appName} appData={appData} />, element);
-    }, i * 2000);
+  document.querySelectorAll('.react-app').forEach(element => {
+    const appName = String(element.getAttribute('data-app'));
+    const appData = getReactAppData(element);
+    ReactDOM.render(<App appName={appName} appData={appData} />, element);
   });
 };
